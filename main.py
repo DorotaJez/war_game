87696e479aa1bd751ce04ecaafd6ebb96f8e2d06
Dorota Jez <depadmil@gmail.com>
2023-04-24 18:26:08 +0100

    added main.py file with basic structures and classes to be developped

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+class Deck:
+    pass
+
+class PlayerCards:
+    pass
+
+class Table:
+    pass
+
+def main():
+    pass
+
+def normal_game():
+    pass
+
+def war_scenario():
+    pass
+
+if __name__ == "__main__":
+    main()
